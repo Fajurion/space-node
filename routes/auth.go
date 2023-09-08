51bf4877d959ec3c5be285d8bbae2579b412aea1
Julian <unfehlerhaft@gmail.com>
2023-09-08 18:06:16 +0200

    Added sender property to prevent /node/connect

--- routes/auth.go
+++ routes/auth.go
@@ -7,7 +7,11 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+const SenderUser = 0
+const SenderNode = 1
+
 type intializeRequest struct {
+	Sender    int    `json:"sender"`  // 0 = user, 1 = node
 	Account   string `json:"account"` // Account ID
 	Session   string `json:"session"` // Room ID in this app
 	NodeToken string `json:"node_token"`
@@ -19,6 +23,10 @@ func initializeConnection(c *fiber.Ctx) error {
 	// Parse the request
 	var req intializeRequest
 	if err := c.BodyParser(&req); err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	if req.Sender == SenderUser {
 		return c.SendStatus(fiber.StatusBadRequest)
 	}
 
